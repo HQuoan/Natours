ab15669319487464a69b175c372abc05ef0f1311
Vo Minh Huy <huyvodtan@gmail.com>
2023-11-06 20:33:15 +0700

    Prepared app for deployment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP headers
 app.use(helmet({ contentSecurityPolicy: false }));
 // app.use(helmet());
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -76,10 +77,12 @@ app.use(
 //     next();
 // });
 
+app.use(compression());
+
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
+  // console.log(req.cookies);
   next();
 });
 
