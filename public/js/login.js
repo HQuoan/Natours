2a003f768aa508989e7ff7eabac2ca6e13f9c405
Vo Minh Huy <huyvodtan@gmail.com>
2023-11-06 18:35:15 +0700

    Initial commit

---
+++ public/js/login.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts.js';
+
+export const login = async (email, password) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    if (response.data.status === 'success') {
+      showAlert('success', 'Logged in successfully!');
+      setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (error) {
+    showAlert('error', error.response.data.message);
+    console.log(error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+    });
+
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch (err) {
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
